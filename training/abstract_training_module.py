cc7054c24af40c27e19e061a6e14c8061c0d4c74
taqiaden <taqiaden@gmail.com>
2026-05-07 13:57:24 +0800

    by taqiaden, portable

--- training/abstract_training_module.py
+++ training/abstract_training_module.py
@@ -769,7 +769,7 @@ class AbstractGraspAgentTraining:
 
             elif ref_success:
                 # if (importance is not None and importance>0.1) or len(self.DDM)<self.max_scenes:
-                importance = 0.5*importance if importance is not None else max(0.01,1-grasp_quality[target_index].item())
+                importance = 0.5*importance if importance is not None else max(0.01,grasp_quality[target_index].item())
                 if importance>0.1:
                     all_pairs.append(
                         (target_index, target_point, grasp_pose_ref_PW[target_index], importance, ref_grasped_obj))
