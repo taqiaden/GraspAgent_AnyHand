c1b9b16fc0ff1b62794aaffaab5098ba46512980
taqiaden <taqiaden@gmail.com>
2026-05-16 15:32:15 +0800

    by taqiaden, portable

--- training/abstract_training_module.py
+++ training/abstract_training_module.py
@@ -736,30 +736,19 @@ class AbstractGraspAgentTraining:
                 return d_pairs, g_pairs, 1
 
             ref_success, ref_initial_collision, ref_n_grasp_contact, ref_self_collide, stable_ref_grasp, warning_flag, ref_plan_found, ref_grasped_obj = self.evaluate_grasp(
-                target_point, target_ref_pose, view=False, shake=False, update_obj_prob=None, check_kinematics=self.check_kinematics)
+                target_point, target_ref_pose, view=False, shake=self.loaded_synthesised_data is not None, update_obj_prob=None, check_kinematics=self.check_kinematics)
 
             if self.loaded_synthesised_data is None or  len(self.loaded_synthesised_data)==0:self.random_sampler_acceptance_rate.update(ref_success)
 
             if warning_flag:
                 break
             gen_success, gen_initial_collision, gen_n_grasp_contact, gen_self_collide, stable_gen_grasp, warning_flag, gen_plan_found, gen_grasped_obj = self.evaluate_grasp(
-                target_point, target_generated_pose, view=False, shake=False, check_kinematics=self.check_kinematics,
+                target_point, target_generated_pose, view=False, shake=self.loaded_synthesised_data is not None, check_kinematics=self.check_kinematics,
                 update_obj_prob=grasp_quality[target_index].item())
 
             if self.check_kinematics:
                 ref_success=ref_success and ref_plan_found
                 gen_success=gen_success and gen_plan_found
-
-            if ref_success and gen_success:
-                '''harder condition: shake'''
-
-                gen_success, gen_initial_collision, gen_n_grasp_contact, gen_self_collide, stable_gen_grasp, warning_flag, gen_plan_found, gen_grasped_obj = self.evaluate_grasp(
-                    target_point, target_generated_pose, view=False, shake=True,
-                    check_kinematics=self.check_kinematics,
-                    update_obj_prob=grasp_quality[target_index].item())
-
-                ref_success, ref_initial_collision, ref_n_grasp_contact, ref_self_collide, stable_ref_grasp, warning_flag, ref_plan_found, ref_grasped_obj = self.evaluate_grasp(
-                    target_point, target_ref_pose, view=False, shake=True, update_obj_prob=None, check_kinematics=self.check_kinematics)
 
             if warning_flag:
                 break
