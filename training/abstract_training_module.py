17693e2d90c9d56d88ee21a5fbb248e5e3e8fbbd
taqiaden <taqiaden@gmail.com>
2026-05-26 20:02:26 +0800

    by taqiaden, portable

--- training/abstract_training_module.py
+++ training/abstract_training_module.py
@@ -811,8 +811,7 @@ class AbstractGraspAgentTraining:
             n = int(min(hh * self.max_n + n, avaliable_iterations))
 
             if len(d_pairs) < self.batch_size and  (ref_success ^ gen_success ):
-                margin = 0 if ref_initial_collision or gen_initial_collision else ((0.5-grasp_quality[target_index]).abs()*2).item()**2
-
+                margin = 0 if ref_initial_collision or gen_initial_collision else (grasp_quality[target_index].item())**2
 
                 d_pairs.append((target_index, k, margin,  target_point))
 
@@ -821,8 +820,8 @@ class AbstractGraspAgentTraining:
                 self.approach_beta_clusters.update(superior_pose[0:5].detach().clone())
 
             if len(g_pairs) < self.batch_size and ref_success and not gen_success:
-                margin = 0 if ref_initial_collision or gen_initial_collision else ((0.5-grasp_quality[target_index]).abs()*2).item()**2
 
+                margin=(1-grasp_quality[target_index].item())**2
 
                 g_pairs.append((target_index, k, margin, target_point))
 
