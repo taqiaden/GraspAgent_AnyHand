35f9051bc49ca83aece680bf0071a97c4de36729
taqiaden <taqiaden@gmail.com>
2026-09-21 09:10:33 +0800

    by taqiaden, portable

--- training/abstract_training_module.py
+++ training/abstract_training_module.py
@@ -1022,12 +1022,12 @@ class AbstractGraspAgentTraining:
                         '''gen_success'''
                         margin =  (0.5 - grasp_quality[target_index]).abs().item() * 2
                         if ref_initial_collision :
-                            margin *=0.1# grasp_feasiblity[target_index].item()
+                            margin =0.1# grasp_feasiblity[target_index].item()
                     else:
                         self.learn_from_heurastic_rate.update(1.0)
                         margin =1-(0.5- grasp_quality[target_index]).abs().item()*2
                         if gen_initial_collision :
-                            margin *=0.1# 1 - grasp_feasiblity[target_index].item()
+                            margin =0.1# 1 - grasp_feasiblity[target_index].item()
 
                     d_sampled_obj_ids.append(grasped_obj)
 
